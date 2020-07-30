bab55ed51f081b15e666e93d3884b28d9a1aa709
gwkim <kking9444@gmail.com>
2020-07-30 11:33:20 +0900

    2020-07-30

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "ROS1과 ROS2의 차이",
+        "excerpt":"ROS1 과 ROS2의 차이 그리고 개념정리 목 차 1. ROS1 과 ROS2의 차이점 비교 2. ROS 1 과 ROS 2 는 통신이 가능한가? 3. ROS 2 는 ROS 1 의 한계를 극복하기위해 만들어 진다 4. ROS 2 와 그 이전의 개념 탑재하기 1. ROS 1 과 ROS 2 의 차이점...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/ros1-ros2/",
+        "teaser": "http://localhost:4000/assets/images/me.jpg"
+      }]
