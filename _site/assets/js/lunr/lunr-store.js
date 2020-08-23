90eea231b7797c32a0b45c111c911b9d2d293f0e
gwkim <kking9444@gmail.com>
2020-08-23 23:25:30 +0900

    2020-08-17

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,5 +3,17 @@ var store = [{
         "excerpt":"목 차 ※ ROS1 과 ROS2 에 대한 차이를 알고 싶으신 분은 1번만 참고하시면 됩니다. 1. ROS1 과 ROS2의 차이점 비교 Application Layer Middleware Layer 지원 OS 지원 언어 기타 2. ROS 1 과 ROS 2 는 통신이 가능한가? 3. ROS 2 는 ROS 1 의 한계를 극복하기위해 만들어 진다...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/ros1-ros2/",
-        "teaser": "http://localhost:4000/assets/images/me.jpg"
+        "teaser": "http://localhost:4000/assets/images/groot.jpeg"
+      },{
+        "title": "ROS2로 ROS1과 통신하는 방법(브릿지)",
+        "excerpt":"ROS1 Bridge 오늘은 ROS1 과 ROS2의 브릿지를 어떻게 연결하는지에대 해 알아보겠다. ROS2는 아직 -ing(현재진행) 형이다. 아직까지 버그는 많고 보완해야될 부분 이 많다. 그리고 그동안 만들어 뒀던 수많은 ROS1에서 만들어진 pkg는 메타운영체제의 플랫폼이 바뀌었다 하여도, 많은 부분에 있어서 다시 제작해서 사용하는것은 있을수 없는 일이다. 애시당초 ROS의 추구 방향은 재사용성 극대화 아니던가?...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/ros2-ros1/",
+        "teaser": "http://localhost:4000/assets/images/groot.jpeg"
+      },{
+        "title": null,
+        "excerpt":"Chapter 03 : 프로세스와 스레드 01. 프로세스의 개념 초기 컴퓨터는 1개의 컴퓨터에 하나의 프로그램이 동작하는 형태의 구조를 가지고있었다. 실행중인 프로그램이 컴퓨터의 모든 자원을 독점하는 이 구조는 불합리하였고, 이를 개선한 다중 프로그래밍 환경에 운영이 가능한 폰 노이만의 하버드 아키텍처(Harvard architecture) 를 채용함으로서 메모리에 여러 프로그램을 적재하는 방식의 컴퓨터 운영방식이 도래하였다. 이에...","categories": [],
+        "tags": null,
+        "url": "http://localhost:4000/2020-08-17-linux-vr-oculus-rift-dk2/",
+        "teaser": "http://localhost:4000/assets/images/groot.jpeg"
       }]
